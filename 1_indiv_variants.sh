eaa6a216607a201803e06dadbc11403d06ab5d8d
rajewski <rajewski23@gmail.com>
2018-09-27 21:50:47 -0700

    fix double single quotes

--- 1_indiv_variants.sh
+++ 1_indiv_variants.sh
@@ -31,7 +31,7 @@ fi
 
 #map the reads
 if [ ! -f "results/"$Indiv".filtered.bam" ]; then
-    ReadGroup=$(echo '\'@RG\tID:'$Indiv'\tSM:'$Indiv'\tLB:Lib\'')
+    ReadGroup=$(echo "\'@RG\tID:'$Indiv'\tSM:'$Indiv'\tLB:Lib\'")
     module load speedseq/a95704a
     speedseq align -t $SLURM_NTASKS -K speedseq.config -o results/$Indiv.filtered -R $ReadGroup references.fasta data/$Sample"_R1_filtered.fastq.gz" data/$Sample"_R2_filtered.fastq.gz"
 fi
