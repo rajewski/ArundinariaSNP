2f3ca884312e5fad5215455451840ece7e23f8b7
rajewski <rajewski23@gmail.com>
2018-10-22 15:15:42 -0700

    change permissions

--- phylo/6b_RAxML.sh
+++ phylo/6b_RAxML.sh
@@ -5,7 +5,7 @@
 #SBATCH --time=08:00:00
 #SBATCH --mail-user=araje002@ucr.edu
 #SBATCH --mail-type=ALL
-#SBATCH -o ../../history/slurm-%A_%a.out
+#SBATCH -o ../history/slurm-%A.out
 
 module load RAxML/8.2.11
-raxmlHPC-PTHREADS-SSE3 -f a -m GTRGAMMAI -p 121787 -x 92191 -N 1000 -T $SLURM_NTASKS -s Plastid_NoMissing.phy -n Plastid.NoMissing.RAxML 
+raxmlHPC-PTHREADS-SSE3 -f a -m GTRGAMMAI -p 121787 -x 92191 -N 1000 -T $SLURM_NTASKS -s WXY/WXY.phy -w ~/bigdata/Arundinaria/phylo/WXY/ -n WXY.RAxML 
