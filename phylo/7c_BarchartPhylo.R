c82e2e3c094e2710826f8148290dd2f4b95617c3
rajewski <rajewski23@gmail.com>
2019-06-03 22:55:38 -0700

    fix color assignment

--- phylo/7c_BarchartPhylo.R
+++ phylo/7c_BarchartPhylo.R
@@ -10,9 +10,9 @@ LFYprops <- LFYprops[,-1]
 LFYprops <- LFYprops[order(match(row.names(LFYprops),LFY$tip.label)),]
 
 #Plot it
-colors<-setNames(c("red","green","blue", "pink", "grey"),c("Appalachiana","Gigantea","Tecta", "Hybrid", "Unknown"))
+colors<-setNames(c("red","green", "pink", "blue", "grey"),c("pApp","pGig", "pHyb", "pTec", "pUnk"))
 #setwd("~/bigdata/Arundinaria/phylo/LFY/HKYI")
-pdf("LFY.pdf", height=20, width=10)
+pdf("~/bigdata/Arundinaria/phylo/LFY/HKYI//LFY.pdf", height=20, width=10)
 #plot(LFY)
   plotTree.barplot(LFY, LFYprops[LFY$tip.label,], args.plotTree = list(show.node.label=T), args.barplot = list(col=colors, main="Species Assignment", border=NA), arg.axis = list(axes=FALSE), legend.text=T)
   legend(x="topright",legend=names(colors), pch=22, pt.cex=2, pt.bg=colors, ncol=3, box.col="transparent")
@@ -32,6 +32,22 @@ WXYprops <- WXYprops[order(match(row.names(WXYprops),WXY$tip.label)),]
 pdf("~/bigdata/Arundinaria/phylo/WXY/GTRGI/WXY.pdf", height=20, width=10)
 #plot(WXY)
   plotTree.barplot(WXY, WXYprops[WXY$tip.label,], args.plotTree = list(show.node.label=T), args.barplot = list(col=colors, main="Species Assignment", border=NA), arg.axis = list(axes=FALSE), legend.text=T)
+  legend(x="topright",legend=names(colors), pch=22, pt.cex=2, pt.bg=colors, ncol=3, box.col="transparent")
+dev.off()
+
+#repeat for plasitd
+plastid <- read.tree("~/bigdata/Arundinaria/phylo/plastid/F81I/Plastid_MrBayes_Collapsed") #import prerooted tree
+
+#read in list of percents
+plastidprops <- read.csv("~/bigdata/Arundinaria/phylo/rename.csv", header = T)[1:26,c(1,8:12)]
+row.names(plastidprops) <- plastidprops[,1]
+plastidprops <- plastidprops[,-1]
+plastidprops <- plastidprops[order(match(row.names(plastidprops),plastid$tip.label)),]
+
+#Plot it
+pdf("~/bigdata/Arundinaria/phylo/plastid/F81I/plastid.pdf", height=20, width=10)
+#plot(plastid)
+  plotTree.barplot(plastid, plastidprops[plastid$tip.label,], args.plotTree = list(show.node.label=T), args.barplot = list(col=colors, main="Species Assignment", border=NA), arg.axis = list(axes=FALSE), legend.text=T)
   legend(x="topright",legend=names(colors), pch=22, pt.cex=2, pt.bg=colors, ncol=3, box.col="transparent")
 dev.off()
 
