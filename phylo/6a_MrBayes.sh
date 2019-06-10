be553cc83c97305a723b0bdb8af21f19f004f664
rajewski <rajewski23@gmail.com>
2019-06-10 16:00:09 -0700

    add history

--- phylo/6a_MrBayes.sh
+++ phylo/6a_MrBayes.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -l
-#SBATCH --ntasks=8
+#SBATCH --ntasks=12
 #SBATCH --nodes=1
 #SBATCH --mem=6G
-#SBATCH --time=2-00:00:00
+#SBATCH --time=9-00:00:00
 #SBATCH --mail-user=araje002@ucr.edu
 #SBATCH --mail-type=ALL
 #SBATCH -p batch
