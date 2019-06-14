5aea8ccf4636c0194c5cd02d8231ffbcfa4947a4
rajewski <rajewski23@gmail.com>
2019-06-14 08:44:15 -0700

    make cophylo plots

--- phylo/7f_Cophylo.R
+++ phylo/7f_Cophylo.R
@@ -1,29 +1,50 @@
-library(ape)
-#on cluster
 setwd("~/bigdata/Arundinaria/phylo/")
-FourFull <- read.tree(file = "FullLength.newick")
-FourColl <- read.tree(file = "FourSpeciesCollapsed.newick")
-ThreeFull <- read.tree(file = "NoRiceFullLength.newick")
-ThreeColl <- read.tree(file = "ThreeSpeciesCollapsed.newick")
-association <- cbind(ThreeFull$tip.label,ThreeFull$tip.label)
 
-#Scale branch lengths for better potting
-FourFull$edge.length <- FourFull$edge.length*100
-ThreeFull$edge.length <- ThreeFull$edge.length*100
-FourColl$edge.length <- FourColl$edge.length*100
-ThreeColl$edge.length <- ThreeColl$edge.length*100
-
-#properly rotate trees
-#this is sort of a kludge because I cant rotate the tree myself using ape, but dont 
-#like the plotting functions of phytools as I understand them, so I've combined them
+library(ape)
 library(phytools)
-comparetrees1 <- cophylo(FourFull, ThreeFull, assoc = association, rotate=TRUE, print=TRUE)
-#save a waypoint
-write.nexus(comparetrees1$trees[[1]], file = "FourFullRot.nex")
-write.nexus(comparetrees1$trees[[2]], file = "ThreeFullRot.nex")
 
-#the tree with polytomies takes waaay longer to sort out
-comparetrees2 <- cophylo(FourColl, ThreeColl, assoc = association, print=TRUE, methods="post", rotate.multi=TRUE)
-#save a waypoint
-write.nexus(comparetrees2$trees[[1]], file = "FourCollRot.nex")
-write.nexus(comparetrees2$trees[[2]], file = "ThreeCollRot.nex")
+# This script runs the same analysis for three loci. It reads in a set of either resolved or collapsed 
+# Bayesian trees, rotates them to align taxon labels, and saves the rotated trees for later plotting. 
+# This is only done if the rotate tree file does not exist in order to save computing time.
+
+# directly importing from MrBayes does not parse the posterior probabilities well. So I am trimming
+# that metadata down with TreeGraph and saving it with branch support (0-1). 
+# I had to manually edit the taxon labels to re-add the underscore in the name of the nexus file, 
+# and it KILLS me to use that program even more.
+
+# Resolved trees for LFY
+if (!file.exists("LFY/HKYI/LFYrenamed.nex.con.rot.tre") && !file.exists("LFY/HKYI/LFYrenamedJimmy.nex.con.rot.tre")) {
+  All <- read.nexus("LFY/HKYI/LFYrenamed.nex.con2.tre") #FIX this with TreeGraph
+  Jimmy <- read.nexus("LFY/HKYI/LFYrenamedJimmy.nex.con2.tre")
+  comparetrees <- cophylo(All, Jimmy, rotate=TRUE, print=TRUE)
+  write.nexus(comparetrees$trees[[1]], file = "LFY/HKYI/LFYrenamed.nex.con.rot.tre")
+  write.nexus(comparetrees$trees[[2]], file = "LFY/HKYI/LFYrenamedJimmy.nex.con.rot.tre")
+  rm(All, Jimmy, comparetrees)
+}
+
+# Resolved trees for WXY
+if (!file.exists("WXY/SYMI/WXYrenamed.nex.con.rot.tre") && !file.exists("WXY/SYMI/WXYrenamedJimmy.nex.con.rot.tre")) {
+  All <- read.nexus("WXY/SYMI/WXYrenamed.nex.con2.tre") 
+  Jimmy <- read.nexus("WXY/SYMI/WXYrenamedJimmy.nex.con2.tre")
+  comparetrees <- cophylo(All, Jimmy, rotate=TRUE, print=TRUE)
+  write.nexus(comparetrees$trees[[1]], file = "WXY/SYMI/WXYrenamed.nex.con.rot.tre")
+  write.nexus(comparetrees$trees[[2]], file = "WXY/SYMI/WXYrenamedJimmy.nex.con.rot.tre")
+  rm(All, Jimmy, comparetrees)
+}
+
+# Resolved plastid trees
+if (!file.exists("plastid/F81I/Plastid_NoMissingrenamed.nex.con.rot.tre") && !file.exists("plastid/F81I/Plastid_NoMissingrenamedJimmy.nex.con.rot.tre")) {
+  All <- read.nexus("plastid/F81I/Plastid_NoMissingrenamed.nex.con2.tre")  
+  Jimmy <- read.nexus("plastid/F81I/Plastid_NoMissingrenamedJimmy.nex.con2.tre")
+  comparetrees <- cophylo(All, Jimmy, rotate=TRUE, print=TRUE)
+  write.nexus(comparetrees$trees[[1]], file = "plastid/F81I/Plastid_NoMissingrenamed.nex.con.rot.tre")
+  write.nexus(comparetrees$trees[[2]], file = "plastid/F81I/Plastid_NoMissingrenamedJimmy.nex.con.rot.tre")
+  rm(All, Jimmy, comparetrees)
+}
+
+# I can't find an R pckage to collapse trees by support, so I am doing it in TreeGraph as well
+# looking at the general cophylo plots, I think I can actually rotate them by hand quicker than the HPCC
+# The algorithm does an exhaustive search of every possible rotation, but by eye only a few corrections are needed.
+# I just did this by hand manually editing the nexus file and saved it (for LFY All only)
+
+
