4962cb6005d878c089fb9864bd3e0f45b2e72211
rajewski <rajewski23@gmail.com>
2019-08-22 13:20:17 -0700

    swap out better concatenated dataset and make plots

--- phylo/7h_SplitsNetworks.R
+++ phylo/7h_SplitsNetworks.R
@@ -4,11 +4,16 @@
 library(phangorn)
 setwd("/bigdata/littlab/arajewski/Arundinaria/phylo/")
 splits <- read.nexus.networx("nuclear/Nuclear_ambig.fasta.nex")
-#splits <- read.nexus.networx("concatenated/Concatenated_ambig.afa.nex")
+#splits <- read.nexus.networx("concatenated/TESTconcatenated.nex.splits")
+# for concatenated only
+splits$tip.label <- gsub(pattern="_", "-", splits$tip.label)
+splits$tip.label <- gsub(pattern="Gig", "Gig9", splits$tip.label)
+splits$tip.label <- gsub(pattern="Tec", "Tec7", splits$tip.label)
 
 # Read key table for label-to-sample matching.
 label_ref <- read.csv("JTCoords.csv", header=T, stringsAsFactor = F)
 label_ref$Species <- as.factor(gsub(pattern = "\\.| ", "", label_ref$Species))
+
 
 # Create species vector by getting IDs from a named vector
 species <- setNames(label_ref$Species, label_ref$Sample)[splits$tip.label]
@@ -25,12 +30,25 @@ species2 <- colmap[as.character(species)]
 
 
 # Write a plot with colored names showing phylo placement.
-pdf(file = "nuclear/NuclearSNPSplits.pdf", width = 6, height = 4)
-#pdf(file = "concatenated/ConcatenatedSNPSplits.pdf", width=6.5, height=6)
+pdf(file = "concatenated/ConcatenatedSNPSplits.pdf", width=7, height=6)
+par(mar = c(0,0,2,0))
+plot(splits, "2D",
+     tip.col = species2,
+     edge.width = 1.5, 
+     cex = 1, 
+     font = 3)
+title(main = "Nuclear and Plastid SNP SplitsTree")
+legend("topleft",legend=c("Hybrid", "A. appalachiana", "A. gigantea", "A. tecta", "Unknown"),col=colmap, pch=16, bty="n")
+dev.off()
 
-par(mar = rep(0, 4))
-plot(splits, "2D", tip.col = species2,
-	edge.width = 1.5, cex = 1, font = 3)
-legend("bottomleft",legend=names(colmap),col=colmap, pch=16, bty="n")
-
+#plot nuclear only snps splitstree
+pdf(file = "nuclear/NuclearSNPSplits.pdf", width = 7, height = 5)
+par(mar = c(0,0,1,0))
+plot(splits, "2D",
+     tip.col = species2,
+     edge.width = 1.5, 
+     cex = 1, 
+     font = 3)
+title(main = "Nuclear SNP SplitsTree")
+legend("bottomleft",legend=c("Hybrid", "A. appalachiana", "A. gigantea", "A. tecta", "Unknown"),col=colmap, pch=16, bty="n")
 dev.off()
--- phylo/7i_SplitsTreeMDS.R
+++ phylo/7i_SplitsTreeMDS.R
@@ -2,9 +2,9 @@ library(phangorn)
 library(ggplot2)
 library(ggrepel)
 library(cowplot)
-setwd("~/Downloads/")
-#x <- read.nexus.dist(file="concatenated_ambig.dist")
-x <- read.nexus.dist(file="nuclear_ambig.dist")
+setwd("/bigdata/littlab/arajewski/Arundinaria/phylo/")
+x <- read.nexus.dist(file="nuclear/nuclear_ambig.dist")
+#x <- read.nexus.dist(file="concatenated/TESTconatenated.dist")
 fit <- cmdscale(x, eig=T, k=2)
 eigenvec <- cbind(rownames(fit$points), fit$points[,1:2])
 
@@ -14,25 +14,24 @@ species$Species <- gsub(pattern = "\\.| " , "", species$Species)
 species$Species <- substr(species$Species,1,4)
 
 #make a color blind friendly palette
-aruncol <- scale_color_manual(breaks = c("Ahybrid", "Aappalachiana", "Agigantea", "Atecta", "Unknown"), 
-                              values = c("#440154FF", "#3B528BFF", "#21908CFF", "#5DC863FF", "#FDE725FF"))
+aruncol <- scale_color_manual(breaks = c("Ahyb", "Aapp", "Agig", "Atec", "Unkn"), 
+                              values = c("#440154FF", "#3B528BFF", "#21908CFF", "#5DC863FF", "#FDE725FF"),
+                              labels = c("Hybrid", "A. appalachiana", "A. gigantea", "A. tecta", "Unknown"))
 
 #merge species names with eigenvector
 mdsdata <- merge(eigenvec, species, by.x="V1", by.y="Sample")
 mdsdata$V2 <- as.numeric(as.character(mdsdata$V2))
 mdsdata$V3 <- as.numeric(as.character(mdsdata$V3))
-#mdsdata$V4 <- as.numeric(as.character(mdsdata$V4))
+mdsdata$Species <- as.factor(mdsdata$Species)
 
-
-#pdf("ConcatenatedMDSSplitsLabeled.pdf", width=11, height=9)
-pdf("NuclearMDSSplitsLabeled.pdf", width=11, height=9)
-ggplot(data = mdsdata, aes(x = V2, y = V3, col=Species)) + 
-  #labs(title="Concatenated SNP MDS (SplitsTree)", x="Dimension 1", y="Dimension 2") +
+#pdf("concatenated/ConcatenatedMDSSplitsLabeled.pdf", width=11, height=9)
+pdf("nuclear/NuclearMDSSplitsLabeled.pdf", width=11, height=9)
+ggplot(data = mdsdata, aes(x = V3, y = V2, color=Species)) + 
+  #labs(title="Nuclear and Plastid SNP MDS (SplitsTree)", x="Dimension 1", y="Dimension 2") +
   labs(title="Nuclear SNP MDS (SplitsTree)", x="Dimension 1", y="Dimension 2") +
   theme(axis.text.x = element_blank(),
         axis.text.y = element_blank(),
-        axis.ticks = element_blank(),
-        legend.position="bottom") +
+        axis.ticks = element_blank()) +
   geom_point(size = 3) +
   geom_text_repel(aes(label = V1)) +
   aruncol
