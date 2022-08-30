c08bfcc9992af7f3bbde0ca8b89328ab171fff3f
rajewski <arajewski@senginemedicine.com>
2022-08-30 15:12:35 -0700

    FASTQ download script and list

---
+++ FASTQ/FASTQ_Download.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Get sample list
+# requires both ffq and jq to be installed
+if [ ! -e ftpLinks.txt ]; then
+    ffq --ftp SRP234396 | jq -r '.[] | .url' > ftpLinks.txt
+fi
+
+# Download samples
+while read -r link; do
+    curl -O $link
+done < ftpLinks.txt
+
