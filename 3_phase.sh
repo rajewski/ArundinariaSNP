0e241d1c7a9ad6408ff0b04d033068f025b93606
rajewski <arajewski@senginemedicine.com>
2022-08-30 18:45:36 -0700

    Potentially fix this non-working typo?

--- 3_phase.sh
+++ 3_phase.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-samtools merge Results/BAM/merged.filtered.bam
-samtools index Results/BAM/merged.filtered.bam
+# Merge individual BAM files
+if [ ! -e Results/BAM/merged.bam ]; then
+  samtools merge Results/BAM/Merged.filtered.bam Results/BAM/*.bam
+fi
 
+# Index the merged BAM
+samtools index Results/BAM/Merged.bam
+
+# Phase the indidivual samples in the merged VCF
 whatshap phase \
   --indels \
   --max-coverage 20 \
   -o Results/SNP/Phased.vcf \
   Results/SNP/Joint.vcf \
-  Results/BAM/merged.filtered.bam
+  Results/BAM/Merged.bam
