74ad178c4d52aed9d52793dbd26dc6569be3fbb6
rajewski <arajewski@senginemedicine.com>
2022-08-30 18:45:57 -0700

    trim fat, but I think this script is broken

--- 4_split_phased.sh
+++ 4_split_phased.sh
@@ -1,19 +1,8 @@
-#!/bin/bash -l
-#SBATCH --ntasks=1
-#SBATCH --nodes=1
-#SBATCH --mem=1G
-#SBATCH --time=2:00:00
-#SBATCH --mail-user=araje002@ucr.edu
-#SBATCH --mail-type=ALL
-#SBATCH -p batch
-#SBATCH -o ./history/slurm-%A_%a.out
+#!/bin/bash 
 
-#Made with help from https://www.biostars.org/p/78929/
+# Made with help from https://www.biostars.org/p/78929/ (Rotten link)
 
-
-module load bcftools/1.8
-
-for file in results/Phased.vcf.gz; do
+for file in Results/SNP/Phased.vcf.gz; do
   for sample in $(bcftools query -l $file) ; do
     bcftools view -c1 -Oz -s "$sample" -o ${file/.vcf*/.$sample.vcf.gz} $file
   done
