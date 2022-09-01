77050ed50976a98c6f254cccc437d4936c85e4ec
rajewski <arajewski@senginemedicine.com>
2022-09-01 15:03:51 -0700

    convert paths for first R script

--- 5_CombinePhaseAmbig.R
+++ 5_CombinePhaseAmbig.R
@@ -1,15 +1,29 @@
-setwd("~/bigdata/Arundinaria/results")
-library(vcfR)
-g <- read.vcfR("Phased.vcf")
+# This script should be run via the R docker container (arundinaRia) after you have sourced the
+# container paths in 0_Paths.env and 0_Containers.env. You can either execute it with ${_arundinaRia[@]}
+# followed by the DOCKER path to the script or interactively run the container by replacing the
+# --entrypoint command
 
+library(vcfR)
+library(Biostrings)
+
+# Read in VCF
+g <- read.vcfR("/root/Results/SNP/Phased.vcf")
 # remove all indels to only deal with SNPs
-g.SNP <- extract.indels(g, return.indels = FALSE)
+g.SNP <- extract.indels(g,
+  return.indels = FALSE
+)
 # Remove null samples
 g.SNP[, -c(75, 76, 89, 90)]
 # make a table of IUPAC ambiguous bases for all heterozygotes
-g.ambig <- alleles2consensus(extract.gt(g.SNP, return.alleles = TRUE), NA_to_n = FALSE)
+g.ambig <- alleles2consensus(extract.gt(g.SNP,
+  return.alleles = TRUE
+),
+NA_to_n = FALSE
+)
 # remove all SNPs, unused right now, but maybe later
-g.InDel <- extract.indels(g, return.indels = TRUE)
+g.InDel <- extract.indels(g,
+  return.indels = TRUE
+)
 
 # Make table of only the phased variants, with unphased as NA
 hapsNA <- extract.haps(g.SNP)
@@ -29,12 +43,10 @@ g.both <- ifelse(is.na(hapsNA), g.ambig, hapsNA)
 g.both <- toupper(g.both) # make them uppercase bc...OCD
 
 # Add these SNPs into the ref sequences
-# source("https://bioconductor.org/biocLite.R")
-# biocLite("Biostrings")
-library(Biostrings)
-refs <- readDNAStringSet("../references.fasta")
+refs <- readDNAStringSet("/root/References/references.fasta")
+
 # make a list to filter out samples we didnt sequence later
-samples <- read.csv("../sampleNames.txt", header = TRUE, sep = "\t")[, 1:2]
+samples <- read.csv("/root/References/sampleNames.txt", header = TRUE, sep = "\t")[, 1:2]
 samples <- paste0(samples$X.target, "_", samples$sample)
 
 # Write WXY Loci
@@ -44,12 +56,19 @@ WXYSNPs <- t(g.both[grep("^WXY", rownames(g.both)), ])
 colnames(WXYSNPs) <- gsub("WXY_", "", colnames(WXYSNPs))
 for (i in 1:148) {
   pos <- WXYSNPs[i, !is.na(WXYSNPs[i, ])]
-  WXYs[[i]] <- replaceLetterAt(WXYs[[i]], 1:length(WXYs[[1]]) %in% as.numeric(names(pos)), as.character(WXYSNPs[i, names(pos)]))
+  WXYs[[i]] <- replaceLetterAt(
+    WXYs[[i]],
+    1:length(WXYs[[1]]) %in% as.numeric(names(pos)),
+    as.character(WXYSNPs[i, names(pos)])
+  )
 }
-wxysamples <- c(paste0(samples[grep("^WXY", samples)], "_0"), paste0(samples[grep("^WXY", samples)], "_1"))
+wxysamples <- c(
+  paste0(samples[grep("^WXY", samples)], "_0"),
+  paste0(samples[grep("^WXY", samples)], "_1")
+)
 wxysamples <- wxysamples[order(wxysamples)]
 WXYs <- WXYs[names(WXYs)[names(WXYs) %in% wxysamples]]
-writeXStringSet(WXYs, "WXY_Phased.fasta")
+writeXStringSet(WXYs, "/root/Results/FASTA/WXY_Phased.fasta")
 
 # Write ambiguous WXY Loci
 WXYsa <- rep(refs["WXY"], 74)
@@ -63,7 +82,7 @@ for (i in 1:74) {
 wxysamplesa <- c(samples[grep("^WXY", samples)])
 wxysamplesa <- wxysamplesa[order(wxysamplesa)]
 WXYsa <- WXYsa[names(WXYsa)[names(WXYsa) %in% wxysamplesa]]
-writeXStringSet(WXYsa, "WXY_ambig.fasta")
+writeXStringSet(WXYsa, "/root/Results/FASTA/WXY_ambig.fasta")
 
 # Write LFY Loci
 LFYs <- rep(refs["LFY"], 148)
@@ -79,7 +98,7 @@ for (i in 1:148) {
 lfysamples <- c(paste0(samples[grep("^LFY", samples)], "_0"), paste0(samples[grep("^LFY", samples)], "_1"))
 lfysamples <- lfysamples[order(lfysamples)]
 LFYs <- LFYs[names(LFYs)[names(LFYs) %in% lfysamples]]
-writeXStringSet(LFYs, "LFY_Phased.fasta")
+writeXStringSet(LFYs, "/root/Results/FASTA/LFY_Phased.fasta")
 
 # Write ambiguous LFY Loci
 LFYsa <- rep(refs["LFY"], 74)
@@ -95,12 +114,12 @@ for (i in 1:74) {
 lfysamplesa <- c(samples[grep("^LFY", samples)])
 lfysamplesa <- lfysamplesa[order(lfysamplesa)]
 LFYsa <- LFYsa[names(LFYsa)[names(LFYsa) %in% lfysamplesa]]
-writeXStringSet(LFYsa, "LFY_ambig.fasta")
+writeXStringSet(LFYsa, "/root/Results/FASTA/LFY_ambig.fasta")
 
 # write nuclear ambiguous
 Nuclearsa <- xscat(LFYsa, WXYsa)
 names(Nuclearsa) <- substr(names(LFYsa), 5, length(names(LFYsa)))
-writeXStringSet(Nuclearsa, "Nuclear_ambig.fasta")
+writeXStringSet(Nuclearsa, "/root/Results/FASTA/Nuclear_ambig.fasta")
 
 # Write matK Loci
 matKs <- rep(refs["matK"], 148)
@@ -114,7 +133,7 @@ for (i in 1:148) {
 matksamples <- c(paste0(samples[grep("^matK", samples)], "_0"), paste0(samples[grep("^matK", samples)], "_1"))
 matksamples <- matksamples[order(matksamples)]
 matKs <- matKs[names(matKs)[names(matKs) %in% matksamples]]
-writeXStringSet(matKs, "matK_Phased.fasta")
+writeXStringSet(matKs, "/root/Results/FASTA/matK_Phased.fasta")
 
 # Write ambiguous matK Loci
 matKsa <- rep(refs["matK"], 74)
@@ -128,7 +147,7 @@ for (i in 1:74) {
 matksamplesa <- c(samples[grep("^matK", samples)])
 matksamplesa <- matksamplesa[order(matksamplesa)]
 matKsa <- matKsa[names(matKsa)[names(matKsa) %in% matksamplesa]]
-writeXStringSet(matKsa, "matK_ambig.fasta")
+writeXStringSet(matKsa, "/root/Results/FASTA/matK_ambig.fasta")
 
 # Write the trnL loci
 trnLs <- rep(refs["trnL"], 148)
@@ -142,7 +161,7 @@ for (i in 1:148) {
 trnlsamples <- c(paste0(samples[grep("^trnL", samples)], "_0"), paste0(samples[grep("^trnL", samples)], "_1"))
 trnlsamples <- trnlsamples[order(trnlsamples)]
 trnLs <- trnLs[names(trnLs)[names(trnLs) %in% trnlsamples]]
-writeXStringSet(trnLs, "trnL_Phased.fasta")
+writeXStringSet(trnLs, "/root/Results/FASTA/trnL_Phased.fasta")
 
 # Write the ambiguous trnL loci
 trnLsa <- rep(refs["trnL"], 74)
@@ -156,7 +175,7 @@ for (i in 1:74) {
 trnlsamplesa <- c(samples[grep("^trnL", samples)])
 trnlsamplesa <- trnlsamplesa[order(trnlsamplesa)]
 trnLsa <- trnLsa[names(trnLsa)[names(trnLsa) %in% trnlsamplesa]]
-writeXStringSet(trnLsa, "trnL_ambig.fasta")
+writeXStringSet(trnLsa, "/root/Results/FASTA/trnL_ambig.fasta")
 
 # Write the psaA loci
 psaAs <- rep(refs["psaA"], 148)
@@ -170,7 +189,7 @@ for (i in 1:148) {
 psaasamples <- c(paste0(samples[grep("^psaA", samples)], "_0"), paste0(samples[grep("^psaA", samples)], "_1"))
 psaasamples <- psaasamples[order(psaasamples)]
 psaAs <- psaAs[names(psaAs)[names(psaAs) %in% psaasamples]]
-writeXStringSet(psaAs, "psaA_Phased.fasta")
+writeXStringSet(psaAs, "/root/Results/FASTA/psaA_Phased.fasta")
 
 # Write the ambiguous psaA loci
 psaAsa <- rep(refs["psaA"], 74)
@@ -184,4 +203,4 @@ for (i in 1:74) {
 psaasamplesa <- c(samples[grep("^psaA", samples)])
 psaasamplesa <- psaasamplesa[order(psaasamplesa)]
 psaAsa <- psaAsa[names(psaAsa)[names(psaAsa) %in% psaasamplesa]]
-writeXStringSet(psaAsa, "psaA_ambig.fasta")
+writeXStringSet(psaAsa, "/root/Results/FASTA/psaA_ambig.fasta")
