005089b5efe18d8f8991f28e4f4873d790d6f916
rajewski <arajewski@senginemedicine.com>
2022-09-02 17:20:27 -0700

    weird straggler. I edited this

--- 6b_RAxML.sh
+++ 6b_RAxML.sh
@@ -7,37 +7,52 @@
 #SBATCH --mail-type=ALL
 #SBATCH -o ../history/RAxML-%A.out
 
-#previous version of RAxML
-#module load RAxML/8.2.11
-#raxmlHPC-PTHREADS-SSE3 -f a -m GTRGAMMAI -p 121787 -x 92191 -N 1000 -T $SLURM_NTASKS -s concatenated/Concatenated_ambig.phy -w ~/bigdata/Arundinaria/phylo/concatenated/GTRGI -n Concatenated_amibg.RAxML 
+# Note: I am not going to make any containers for the phylogenetic softwares. They are simply too complex.
+# Because I will have no way to run them locally and test, I am not going to really modify these scripts
+# except to flesh out parts that I ran interactively with more thorough code. For that reason, you will 
+# need to be careful about the paths that are used. I have left local paths in  place, but they could 
+# be a source of trouble later for you.
+
+# Get paths
+source 0_Containers.env
 
 module load RAxML_NG/0.9.0
 cd /rhome/arajewski/bigdata/Arundinaria/phylo/plastid/GTRGI || exit
-ALIGNIN=Plastid_NoMissing_rpl32.phy
+ALIGNIN="${path_results_local}/Results/PHYLO/Plastid_NoMissing_rpl32.phy"
 
+# Parse the alignment and estimate the mem and CPU reqs.
 if [ ! -e "$ALIGNIN.raxml.rba" ]; then
-    #command to parse the alignment and estimate the mem and CPU reqs.
     echo "$(date): Estimating time to completion."
     raxml-ng --parse \
-	--msa ../$ALIGNIN \
+	--msa $ALIGNIN \
 	--model GTR+G+I \
-	--prefix ./$ALIGNIN
+	--prefix $ALIGNIN
     echo "$(date): Done."
 else
     echo "$(date): Parsing already complete"
 fi
 
-#actualy run the analysis
+# Actualy run the analysis
 echo "$(date): Beginning RAxML estimation."
 raxml-ng --all \
     --tree pars{25},rand{25} \
-    --msa ../$ALIGNIN \
-    --prefix ./$ALIGNIN \
+    --msa $ALIGNIN \
+    --prefix $ALIGNIN \
     --model GTR+G+I \
     --outgroup 'P.edulis' \
     --seed 121787 \
     --blopt nr_safe \
     --threads "$SLURM_NTASKS"
 
-#print job information for later reference
-scontrol show job "$SLURM_JOB_ID"
+# previous version of RAxML
+# module load RAxML/8.2.11
+# raxmlHPC-PTHREADS-SSE3 \
+#   -f a \
+#   -m GTRGAMMAI \
+#   -p 121787 \
+#   -x 92191 \
+#   -N 1000 \
+#   -T $SLURM_NTASKS \
+#   -s concatenated/Concatenated_ambig.phy \
+#   -w ~/bigdata/Arundinaria/phylo/concatenated/GTRGI \
+#   -n Concatenated_amibg.RAxML 
